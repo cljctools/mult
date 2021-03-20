ff486cd3a2133982682a6f9563a01850b2cd98ca
Sergei Udris <sergei.udris@gmail.com>
2021-03-20 23:24:51 +0300

    remove lsp
    - it has a miriade of useless features, and no useful
    - and it's in the wrong direction of static analisys, linting bureaucracy instead of reasoning about program meaning
    - what is needed is http-repl for evaluation, lsp only makes sense for non-lisp static languages
    - we need formatiing, color brackets and tokens, selection on double-click and paredit commands to select/move forms, else is unhealthy barriers and noise

--- src/mult-vscode/src/cljctools/mult/vscode/main.cljs
+++ src/mult-vscode/src/cljctools/mult/vscode/main.cljs
@@ -21,8 +21,6 @@
    [cljctools.mult.format.spec :as mult.format.spec]
    [cljctools.mult.format.core :as mult.format.core]
 
-   [cljctools.mult.lsp.core :as mult.lsp.core]
-
    [cljctools.mult.protocols :as mult.protocols]
    [cljctools.mult.spec :as mult.spec]
    [cljctools.mult.core :as mult.core]))
@@ -46,7 +44,6 @@
                                             ::mult.format.spec/fmt fmt
                                             ::mult.spec/config config
                                             ::mult.editor.spec/editor editor})]
-      (<! (mult.lsp.core/activate context))
       (mult.editor.core/register-commands*
        editor
        {::mult.editor.core/cmds {::mult.spec/cmd-open {::mult.editor.core/cmd-id ":cljctools.mult.spec/cmd-open"}
@@ -67,7 +64,6 @@
   []
   (go
     (when-let [editor (get @registryA ::editor)]
-      (<! (mult.lsp.core/deactivate))
       (mult.core/release ::mult)
       (mult.format.core/release ::mult-format)
       (mult.editor.protocols/release* editor)
